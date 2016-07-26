30d672844e5d7e0ec8d25f64709cb158afc4050a
mohammad taghipoor <mohammad taghipoor>
2016-07-26 14:54:41 +0430

    add capability to change authentication type to use username instead of email. closes #20

---
+++ src/Plugin.Tests/PayamGostarPluginTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Moq;
+using Xunit;
+using Nop.Services.Configuration;
+using Nop.Services.Stores;
+using Nop.Core;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Plugins;
+
+namespace Septa.PgNopIntegration.Plugin.Tests
+{
+    public class PayamGostarPluginTest
+    {
+        [Fact]
+        public void UsernameAuthenticationModeIsEnabledAfterPluginIsInstalled()
+        {
+            // Arrange
+            var settingServiceMock = new Mock<ISettingService>();
+            var storeServiceMock = new Mock<IStoreService>();
+            var workContextMock = new Mock<IWorkContext>();
+
+            settingServiceMock.Setup(x => x.LoadSetting<CustomerSettings>(It.IsAny<int>())).
+                Returns(new CustomerSettings() { UsernamesEnabled = true });
+
+            storeServiceMock.Setup(x => x.GetAllStores().Count)
+                .Returns(1);
+
+            BasePlugin payamGostarPlugin = new PayamGostarPlugin(
+                settingServiceMock.Object,
+                storeServiceMock.Object,
+                workContextMock.Object);
+
+            // Act
+            payamGostarPlugin.Install();
+
+            // Assert
+            settingServiceMock.Verify(x => x.LoadSetting<CustomerSettings>(It.IsAny<int>()).UsernamesEnabled == true);
+        }
+    }
+}
---
+++ src/Plugin/PayamGostarPlugin.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Nop.Core.Plugins;
+using Nop.Services.Configuration;
+using Nop.Services.Stores;
+using Nop.Core;
+using Nop.Services.Common;
+using Nop.Core.Domain.Customers;
+
+namespace Septa.PgNopIntegration.Plugin
+{
+    /// <summary>
+    /// PayamGostar Plugin
+    /// </summary>
+    public class PayamGostarPlugin : BasePlugin
+    {
+        # region Fields
+
+        private readonly ISettingService _settingService;
+        private readonly IStoreService _storeService;
+        private readonly IWorkContext _workContext;
+
+        # endregion
+
+        # region Ctor
+
+        public PayamGostarPlugin(ISettingService settingService, IStoreService storeService, IWorkContext workContext)
+        {
+            this._settingService = settingService;
+            this._storeService = storeService;
+            this._workContext = workContext;
+        }
+
+        # endregion
+
+        # region Utilities
+
+        /// <summary>
+        /// enables username authenticaion mode instead of email authentication mode
+        /// </summary>
+        private void EnableUsernameAuthenticationMode()
+        {
+            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var customerSettings = _settingService.LoadSetting<CustomerSettings>(storeScope);
+
+            customerSettings.UsernamesEnabled = true;
+
+            _settingService.SaveSetting(customerSettings);
+        }
+
+        /// <summary>
+        /// gets configuration scope for active store
+        /// </summary>
+        private int GetActiveStoreScopeConfiguration(IStoreService storeService, IWorkContext workContext)
+        {
+            //ensure that we have 2 (or more) stores
+            if (storeService.GetAllStores().Count < 2)
+                return 0;
+
+            var storeId = workContext.CurrentCustomer.GetAttribute<int>(SystemCustomerAttributeNames.AdminAreaStoreScopeConfiguration);
+            var store = storeService.GetStoreById(storeId);
+            return store != null ? store.Id : 0;
+        }
+
+        # endregion
+
+        # region Methods
+
+        /// <summary>
+        /// Install plugin
+        /// </summary>
+        public override void Install()
+        {
+            base.Install();
+            EnableUsernameAuthenticationMode();
+
+        }
+
+        /// <summary>
+        /// Uninstall plugin
+        /// </summary>
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+
+        # endregion
+    }
+}
