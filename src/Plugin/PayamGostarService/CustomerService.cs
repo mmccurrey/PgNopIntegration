e5125f5d582c9f0763fcb56869a7004d2f25411d
mohammad taghipoor <mohammad taghipoor>
2016-08-14 18:56:16 +0430

    closes #21 check if username or email is existed in pg or not!

---
+++ src/Plugin/PayamGostarService/CustomerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Septa.PayamGostar.ServiceLayer.ServiceType.Api;
+
+namespace Septa.PgNopIntegration.Plugin.PayamGostarService
+{
+    /// <summary>
+    /// CustomerService channel
+    /// </summary>
+    public class CustomerService : ICustomerService
+    {
+        /// <summary>
+        /// Returns true when a customer(user) exists in Payam Gostar customers(users)
+        /// </summary>
+        /// <param name="username">Username</param>
+        /// /// <param name="email">Email</param>
+        /// <returns>bool</returns>
+        public bool IsRegistered(string username, string email)
+        {
+            return new bool();
+        }
+    }
+}
---
+++ src/Plugin/PayamGostarService/ICustomerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Septa.PayamGostar.ServiceLayer.ServiceType.Api
+{
+    /// <summary>
+    /// Service contract for Payam Gostar service which handles customers(users) functionalities from and to nop commerce
+    /// </summary>
+    [ServiceContract]
+    public interface ICustomerService
+    {
+        /// <summary>
+        /// Returns true when a customer(user) exists in Payam Gostar customers(users)
+        /// </summary>
+        /// <param name="username">Username</param>
+        /// /// <param name="email">Email</param>
+        /// <returns>bool</returns>
+        [OperationContract]
+        bool IsRegistered(string username, string email);
+    }
+}
